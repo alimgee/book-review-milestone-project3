9aaefb0107ca6fdc94360ec356876cf304af51c6
Alan Mc Gee <mcgee.al@gmail.com>
2019-08-03 20:35:20 +0100

    Adding mongodb database to app Modified app to return the users and reviews collections to index Pulled username fields from users and reviews collections to test All tests passed - Mongo fully connected to app

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+# passing mongodb uri via environment 
+app.config["MONGO_DBNAME"] = "booksDB"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+# passing Secret key via environment 
 app.secret_key = ('SECRET_KEY')
+#creating mongo app
+mongo = PyMongo(app)
 
-
-
-#setting default root
-# testing flask
+# landing route - passing both collections to index.html to test
 @app.route('/')
 def index(): 
-    return render_template("index.html")
-
+    users=mongo.db.users.find()
+    reviews=mongo.db.reviews.find()
+    return render_template("index.html", users=users, reviews = reviews)
 
 
 
