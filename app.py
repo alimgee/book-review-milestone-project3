00088623d72890db8325ff190ddd7cff43cd8aa6
Alan Mc Gee <mcgee.al@gmail.com>
2019-08-07 17:11:42 +0100

    Creaitng Review page: Adding  route for Review, verify db content been pulled in review. Modified title field

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo, pymongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 # passing mongodb uri via environment 
@@ -13,10 +14,19 @@ mongo = PyMongo(app)
 
 # landing page route
 @app.route('/')
-def index(): 
+def index():
+    ''' function to display all records on the landing page''' 
     users=mongo.db.users.find()
     reviews=mongo.db.reviews.find().sort('upvote', pymongo.DESCENDING)# sort reviews by popularity (upvote)
     return render_template("index.html", users=users, reviews = reviews)
+
+@app.route('/review/<id>', methods=['GET', 'POST'])
+def review(id):
+    ''' function to return a single record of the review db
+     on the basis of the id of the item in the collection'''
+    one_review = mongo.db.reviews.find_one({"_id": ObjectId(id)})
+    title = one_review['book_title']
+    return render_template("review.html",  review = one_review, title =title)
 
 
 
