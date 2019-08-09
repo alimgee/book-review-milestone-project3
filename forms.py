088e4909dbb17c45d5a975e1753c28964a42e5b1
Alan Mc Gee <mcgee.al@gmail.com>
2019-08-09 14:21:36 +0100

    Adding proper form fields to register.html and styling

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('username' , validators =[DataRequired() , Length(min=3, max =20)])
-    password = PasswordField('password' , validators =[DataRequired()])
+    username = StringField('Username' , validators =[DataRequired() , Length(min=3, max =20)])
+    password = PasswordField('Password' , validators =[DataRequired()])
     confirm_password = PasswordField('Confirm Password' , validators =[DataRequired() , EqualTo('password')])
     submit = SubmitField('Register Now')
