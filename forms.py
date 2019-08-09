27214c7ebb09b963f07d448fba3424de46268537
Alan Mc Gee <mcgee.al@gmail.com>
2019-08-09 14:08:01 +0100

    Missed some changes on last commit - redoing

--- forms.py
+++ forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('username' , validators =[DataRequired() , Length(min=3, max =20)])
-    password = PasswordField('Password' , validators =[DataRequired()])
+    password = PasswordField('password' , validators =[DataRequired()])
     confirm_password = PasswordField('Confirm Password' , validators =[DataRequired() , EqualTo('password')])
     submit = SubmitField('Register Now')
