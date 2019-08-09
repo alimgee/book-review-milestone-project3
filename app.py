295c31b19c5ed056b500fb8dbd76b9a53530d051
Alan Mc Gee <mcgee.al@gmail.com>
2019-08-09 13:29:11 +0100

    Installing flask-wtf and created forms.py Adding registration form to forms .py Adding favicon

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
+from forms import RegistrationForm
 
 app = Flask(__name__)
 # passing mongodb uri via environment 
